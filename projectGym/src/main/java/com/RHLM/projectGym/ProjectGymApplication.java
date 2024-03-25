d913d2a97637be3495cd5eb3ad6dfba42fcb7e51
sebastian-suarez-2191933 <91213545+sebastian-suarez-2191933@users.noreply.github.com>
2024-03-25 17:27:37 -0500

    First Commit
    Commit del proyecto base de SprignBoot.

---
+++ projectGym/src/main/java/com/RHLM/projectGym/ProjectGymApplication.java
@@ -0,0 +1,13 @@
+package com.RHLM.projectGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGymApplication.class, args);
+	}
+
+}
---
+++ projectGym/src/test/java/com/RHLM/projectGym/ProjectGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RHLM.projectGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
